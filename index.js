59a3af1e7f3f9ad014cfb391c4682788c91db37b
Vipul7876 <vipulsharma7876@gmail.com>
2024-08-11 11:45:49 +0530

    adding origins

--- index.js
+++ index.js
@@ -6,15 +6,30 @@ const Router = require( './Routes/Route' );
 const Connection = require( './Models/Database' );
 
 // Middleware
-const corsoptions = {
-  // origin: "http://localhost:5173",
-  origin: "https://blogg01.netlify.app",
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://blogg01.netlify.app",
+  "https://66b84774b696fb00081531c7--blogg01.netlify.app",
+];
+
+const corsOptions = {
+  origin: function ( origin, callback ) {
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if ( !origin ) return callback( null, true );
+
+    if ( allowedOrigins.indexOf( origin ) !== -1 ) {
+      callback( null, true ); // If the origin is in the list, allow the request
+    } else {
+      callback( new Error( "Not allowed by CORS" ) ); // If the origin is not in the list, block the request
+    }
+  },
   methods: "GET,POST,PATCH,PUT,DELETE,HEAD",
   credentials: true,
   exposedHeaders: [ 'Authorization' ],
 };
 
-app.use( CORS( corsoptions ) );
+app.use( CORS( corsOptions ) );
 
 app.use( express.json() );
 
